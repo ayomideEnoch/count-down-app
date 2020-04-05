cd85b34d0aae2128cd1e42438b1f1890d2b79a0f
Ayomide Ibosiola <Ayomide Ibosiola>
2020-04-05 12:19:48 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Countdown from './components/Countdown';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Countdown />
+			</div>
+		);
+	}
+}
+
+export default App;
